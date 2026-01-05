fe2a8325f2f925637d85e682383d1d73cdfa88e2
hustlenomicsorg <hustlenomics.info@gmail.com>
2026-01-05 14:16:41 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,28 @@
 import "./globals.css";
+import { Montserrat, Nunito } from "next/font/google";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["600", "700", "800"],
+  variable: "--font-heading",
+});
+
+const nunito = Nunito({
+  subsets: ["latin"],
+  weight: ["400", "500", "600"],
+  variable: "--font-body",
+});
 
 export default function RootLayout({
-  children
+  children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${montserrat.variable} ${nunito.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
